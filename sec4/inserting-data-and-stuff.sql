21e0c4c6d09cd21593812842293d2e556c447f85
codecamjam <camerontaghlabi@gmail.com>
2021-07-27 11:12:44 -0400

    l45 and l47 insert challenge

--- sec4/inserting-data-and-stuff.sql
+++ sec4/inserting-data-and-stuff.sql
@@ -32,8 +32,52 @@ VALUES ('Peanut', 2),
 -- Query OK, 3 rows affected (0.01 sec)
 -- Records: 3  Duplicates: 0  Warnings: 0
 
-/*l 
+
+/*l45 and l147 INSERT challenge
 ****************************************************/
+/* 
+Create a people table
+first_name 20 char limit,
+last_name 20 char limit,
+age
+
+Insert 1 at a time:
+Tina Belcher 13
+Bob Belcher 42
+
+Multiple insert
+Linda Belcher 45
+Phillip Frond 38
+Calvin Fischoeder 70
+*/
+
+CREATE TABLE people(
+  first_name CHAR(20),
+  last_name CHAR(20),
+  age INT
+);
+
+DESC people;
+
+INSERT INTO people
+  (first_name, last_name, age)
+VALUES 
+  ('Tina', 'Belcher', 13);
+
+INSERT INTO people
+  (age, last_name, first_name)
+VALUES 
+  (42, 'Belcher', 'Bob');
+
+INSERT INTO people
+  (first_name, last_name, age)
+VALUES 
+  ('Linda', 'Belcher', 45),
+  ('Phillip', 'Frond', 38),
+  ('Calvin', 'Fischoeder', 70);
+
+SELECT * FROM people;
+DROP TABLE people;
 
 /*l 
 ****************************************************/
