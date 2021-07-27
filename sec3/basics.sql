b530a6b88ab6a98d9ecf8c0d89381513fd519daa
codecamjam <camerontaghlabi@gmail.com>
2021-07-27 10:34:52 -0400

    l36 creating your own tables challenge

--- sec3/basics.sql
+++ sec3/basics.sql
@@ -22,28 +22,49 @@ SELECT database();
    ROWS - data
 */
 
+
 /*l26 data types ****************************************************/
 /*varchar(100) - must specify <= length, and max is 255 */
+
 
 /*l30 creating tables ****************************************************/
 CREATE DATABASE cat_app;
 USE cat_app;
-
 CREATE TABLE cats (
   name VARCHAR(100),
   age INT
 );
+
 
 /*l32 how we know it worked ****************************************************/
 SHOW TABLES;
 SHOW COLUMNS FROM cats;
 DESC cats; /*not same as show columns from but in this context, its the same */
 
+
 /*l34 drop tables ****************************************************/
 DROP TABLE cats;
 
-/*l ****************************************************/
 
+/*l36 create own table ****************************************************/
+/* create a pastries table:
+  -it should include 2 columns: name and quantity.
+  -Name is 50 chars max. 
+  -Inspect your table/columns in the cli
+  -delete the table
+*/
+CREATE DATABASE pastry_shop;
+USE pastry_shop;
+SELECT database();
+CREATE TABLE pastries(
+  name VARCHAR(50),
+  quantity INT
+);
+SHOW TABLES;
+SHOW COLUMNS FROM pastries;
+DESC pastries;
+DROP TABLE pastries;
+SHOW TABLES;
 
 /*l ****************************************************/
 /*l ****************************************************/
