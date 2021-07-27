143fcdfdce940c6ed13eebe34f15001490ea9ad5
codecamjam <camerontaghlabi@gmail.com>
2021-07-27 16:49:35 -0400

    l107 reverse

--- sec7/string-functions.sql
+++ sec7/string-functions.sql
@@ -122,9 +122,17 @@ FROM books;
 
 ------------------------------------------------------
 
-/*l
+/*l107 reverse
 *****************************************************/
-
+SELECT REVERSE('Hello World');
+ 
+SELECT REVERSE('meow meow');
+ 
+SELECT REVERSE(author_fname) FROM books;
+ 
+SELECT CONCAT('woof', REVERSE('woof'));
+ 
+SELECT CONCAT(author_fname, REVERSE(author_fname)) FROM books;
 
 
 ------------------------------------------------------
