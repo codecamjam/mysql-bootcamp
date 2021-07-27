4732ecf252852138c6a8aef6b2fe9e5d44d5a2eb
codecamjam <camerontaghlabi@gmail.com>
2021-07-27 16:37:05 -0400

    l103 substring

--- sec7/string-functions.sql
+++ sec7/string-functions.sql
@@ -5,7 +5,7 @@ source book_data.sql;
 
 ------------------------------------------------------
 
-/*l01 concat - combine data for cleaner output
+/*l101 concat - combine data for cleaner output
 *****************************************************/
 SELECT CONCAT('Hello', 'World');
  
@@ -37,10 +37,54 @@ FROM books;
 
 ------------------------------------------------------
 
-/*l
+/*l103 substring
 *****************************************************/
+SELECT SUBSTRING('Hello World', 1, 4);
+--Hell
 
+SELECT SUBSTRING('Hello World', 7);
+--World
 
+SELECT SUBSTRING('Hello World', -3);
+--rld
+
+SELECT SUBSTRING('Hello World', 1, 4);
+ 
+SELECT SUBSTRING('Hello World', 7);
+ 
+SELECT SUBSTRING('Hello World', 3, 8);
+ 
+SELECT SUBSTRING('Hello World', 3);
+ 
+SELECT SUBSTRING('Hello World', -3);
+ 
+SELECT SUBSTRING('Hello World', -7);
+ 
+SELECT title FROM books;
+ 
+SELECT SUBSTRING("Where I'm Calling From: Selected Stories", 1, 10);
+ 
+SELECT SUBSTRING(title, 1, 10) FROM books;
+ 
+SELECT SUBSTRING(title, 1, 10) AS 'short title' FROM books;
+ 
+SELECT SUBSTR(title, 1, 10) AS 'short title' FROM books;
+ 
+SELECT CONCAT
+  (
+    SUBSTRING(title, 1, 10),
+    '...'
+  )
+FROM books;
+ 
+source book_code.sql
+ 
+SELECT CONCAT
+  (
+    SUBSTRING(title, 1, 10),
+    '...'
+  ) AS 'short title'
+FROM books;
 
 ------------------------------------------------------
 
