72503ea6a8257b83bb0fb74f2168873262802762
codecamjam <camerontaghlabi@gmail.com>
2021-07-27 16:57:25 -0400

    l111 upper and lower

--- sec7/string-functions.sql
+++ sec7/string-functions.sql
@@ -155,9 +155,17 @@ FROM books;
 
 ------------------------------------------------------
 
-/*l
+/*l111 upper and lower
 *****************************************************/
-
+SELECT UPPER('Hello World');
+ 
+SELECT LOWER('Hello World');
+ 
+SELECT UPPER(title) FROM books;
+ 
+SELECT CONCAT('MY FAVORITE BOOK IS ', UPPER(title)) FROM books;
+ 
+SELECT CONCAT('MY FAVORITE BOOK IS ', LOWER(title)) FROM books;
 
 
 ------------------------------------------------------
