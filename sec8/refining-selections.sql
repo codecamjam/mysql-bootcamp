39e3cb18d4f98b32442ab2161636147b6f9ba29b
codecamjam <camerontaghlabi@gmail.com>
2021-08-02 19:37:58 -0400

    l120 distinct

--- sec8/refining-selections.sql
+++ sec8/refining-selections.sql
@@ -9,10 +9,17 @@ INSERT INTO books
 
 ------------------------------------------------------
 
-/*l
+/*l120 distinct
 *****************************************************/
-
-
+SELECT author_lname FROM books;
+ 
+SELECT DISTINCT author_lname FROM books;
+ 
+SELECT author_fname, author_lname FROM books;
+ 
+SELECT DISTINCT CONCAT(author_fname,' ', author_lname) FROM books;
+ 
+SELECT DISTINCT author_fname, author_lname FROM books;
 
 ------------------------------------------------------
 
