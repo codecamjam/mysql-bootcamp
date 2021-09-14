9f1153c69b744104a06e9844802127b32019e772
codecamjam <camerontaghlabi@gmail.com>
2021-09-14 15:17:37 -0400

    l130 refining selections exercises

--- sec8/refining-selections.sql
+++ sec8/refining-selections.sql
@@ -128,18 +128,101 @@ SELECT title FROM books WHERE title LIKE '%the%';
 ------------------------------------------------------
 
 
-/*l
+/*l28 LIKE pt 2: more wildcards
 *****************************************************/
-
+SELECT title, stock_quantity FROM books;
+ 
+SELECT title, stock_quantity FROM books WHERE stock_quantity LIKE '____';
+ 
+SELECT title, stock_quantity FROM books WHERE stock_quantity LIKE '__';
+ 
+(235)234-0987 LIKE '(___)___-____'
+ 
+SELECT title FROM books;
+ 
+SELECT title FROM books WHERE title LIKE '%\%%'
+ 
+SELECT title FROM books WHERE title LIKE '%\_%'
 
 
 ------------------------------------------------------
 
 
-/*l
+/*l130 refining selections challenge
 *****************************************************/
 
+---select all story collections (titles that contain 'stories')
+SELECT title 
+FROM books 
+WHERE title 
+LIKE '%stories%';
+-- +-----------------------------------------------------+
+-- | title                                               |
+-- +-----------------------------------------------------+
+-- | What We Talk About When We Talk About Love: Stories |
+-- | Where I'm Calling From: Selected Stories            |
+-- | Oblivion: Stories                                   |
+-- +-----------------------------------------------------+
 
+---find the longest book (print out the title and page count)
+SELECT title, pages 
+FROM books 
+ORDER BY pages 
+DESC LIMIT 1;
+-- +-------------------------------------------+-------+
+-- | title                                     | pages |
+-- +-------------------------------------------+-------+
+-- | The Amazing Adventures of Kavalier & Clay |   634 |
+-- +-------------------------------------------+-------+
+
+-- Print a summary containing the title and year,
+-- for the 3 most recent books
+SELECT CONCAT(title, ' - ', released_year) 
+AS summary 
+FROM books 
+ORDER BY released_year
+DESC LIMIT 3;
+-- +-----------------------------+
+-- | summary                     |
+-- +-----------------------------+
+-- | Lincoln In The Bardo - 2017 |
+-- | Norse Mythology - 2016      |
+-- | 10% Happier - 2014          |
+-- +-----------------------------+
+
+---find all books with an author_lname that contains
+-- a space(' ')
+SELECT title, author_lname 
+FROM books 
+WHERE author_lname 
+LIKE '% %';
+-- +----------------------+----------------+
+-- | title                | author_lname   |
+-- +----------------------+----------------+
+-- | Oblivion: Stories    | Foster Wallace |
+-- | Consider the Lobster | Foster Wallace |
+-- +----------------------+----------------+
+
+-- find the 3 books with the lowest stock 
+-- (select title, year, and stock)
+SELECT title, released_year, stock_quantity
+FROM books
+ORDER BY stock_quantity
+ASC LIMIT 3;
+
+-- print title and author_lname, sorted first
+-- by author_lname and then by title
+SELECT title, author_lname 
+FROM books
+ORDER BY author_lname, title;
+
+SELECT
+    UPPER(
+        CONCAT('My favorite author is ', author_fname, ' ', author_lname, '!')
+    ) 
+AS yell
+FROM books
+ORDER BY author_lname;
 
 ------------------------------------------------------
 
