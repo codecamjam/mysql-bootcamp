5950966f98c20501e08f9e1990b30feabed74e63
codecamjam <camerontaghlabi@gmail.com>
2022-03-05 12:27:22 -0500

    l162 date, time, and datetime AND l163 creating our date data

--- sec10/revisiting-datatypes.sql
+++ sec10/revisiting-datatypes.sql
@@ -62,16 +62,18 @@ SELECT * FROM thingies;
 ------------------------------------------------------
 
 
-/*l162 date, time, and datetime
+
+/*l162 date, time, and datetime AND l163 creating our date data
 *****************************************************/
-
-
-
-------------------------------------------------------
-
-
-/*l163 creating our date data
-*****************************************************/
+CREATE TABLE people (name VARCHAR(100), birthdate DATE, birthtime TIME, birthdt DATETIME);
+ 
+INSERT INTO people (name, birthdate, birthtime, birthdt)
+VALUES('Padma', '1983-11-11', '10:07:35', '1983-11-11 10:07:35');
+ 
+INSERT INTO people (name, birthdate, birthtime, birthdt)
+VALUES('Larry', '1943-12-25', '04:10:42', '1943-12-25 04:10:42');
+ 
+SELECT * FROM people;
 
 
 
