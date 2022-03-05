8da2e8e53860682babce6ad7695e6318c5f5840a
codecamjam <camerontaghlabi@gmail.com>
2022-03-05 13:47:01 -0500

    l187 logical or

--- sec11/logical-operators.sql
+++ sec11/logical-operators.sql
@@ -151,7 +151,32 @@ WHERE author_lname='Eggers'
 
 /*l187 logical or
 *****************************************************/
-
+-- || IS DEPRECATED, USE OR INSTEAD
+SELECT 
+    title, 
+    author_lname, 
+    released_year 
+FROM books
+WHERE author_lname='Eggers' OR released_year > 2010;
+ 
+ 
+SELECT 40 <= 100 OR -2 > 0;
+-- true
+ 
+SELECT 10 > 5 OR 5 = 5;
+-- true
+ 
+SELECT 'a' = 5 OR 3000 > 2000;
+-- true
+ 
+SELECT title, 
+       author_lname, 
+       released_year, 
+       stock_quantity 
+FROM   books 
+WHERE  author_lname = 'Eggers' 
+              OR released_year > 2010 
+OR     stock_quantity > 100;
 
 
 ------------------------------------------------------
