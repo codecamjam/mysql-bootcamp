4fb0ba7eff904ebb24c7c6b0e9b3e284fa57cf46
codecamjam <camerontaghlabi@gmail.com>
2022-03-04 23:06:19 -0500

    l148 avg function

--- sec9/aggregateFunctions.sql
+++ sec9/aggregateFunctions.sql
@@ -160,16 +160,32 @@ ORDER BY sum_pages;
 ------------------------------------------------------
 
 
-/*l
+/*l148 AVG Function
 *****************************************************/
-
+SELECT AVG(released_year) 
+FROM books;
+ 
+SELECT AVG(pages) 
+FROM books;
+ 
+SELECT AVG(stock_quantity) 
+FROM books 
+GROUP BY released_year;
+ 
+SELECT released_year, AVG(stock_quantity) 
+FROM books 
+GROUP BY released_year;
+ 
+SELECT author_fname, author_lname, AVG(pages) FROM books
+GROUP BY author_lname, author_fname;
 
 
 ------------------------------------------------------
 
 
-/*l
+/*l150 Aggregate function challenges
 *****************************************************/
+
 
 
 
