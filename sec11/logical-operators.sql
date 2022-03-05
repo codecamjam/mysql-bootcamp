b5540d775f0f45bd7438ae6d863a6d2646ef4050
codecamjam <camerontaghlabi@gmail.com>
2022-03-05 13:40:32 -0500

    l183 less than

--- sec11/logical-operators.sql
+++ sec11/logical-operators.sql
@@ -83,7 +83,28 @@ SELECT title, author_lname FROM books WHERE author_lname = 'eGGers';
 
 /*l183 less than
 *****************************************************/
-
+SELECT title, released_year FROM books;
+ 
+SELECT title, released_year FROM books
+WHERE released_year < 2000;
+ 
+SELECT title, released_year FROM books
+WHERE released_year <= 2000;
+ 
+SELECT 3 < -10;
+-- false
+ 
+SELECT -10 < -9;
+-- true
+ 
+SELECT 42 <= 42;
+-- true
+ 
+SELECT 'h' < 'p';
+-- true
+ 
+SELECT 'Q' <= 'q';
+-- true
 
 
 ------------------------------------------------------
