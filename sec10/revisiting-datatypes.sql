e9efdfe312234751d9617d8a986701fbcd407aaa
codecamjam <camerontaghlabi@gmail.com>
2022-03-05 12:15:10 -0500

    l157 decimal

--- sec10/revisiting-datatypes.sql
+++ sec10/revisiting-datatypes.sql
@@ -21,7 +21,20 @@ SELECT * FROM dogs;
 
 /*l157 decimal
 *****************************************************/
-
+CREATE TABLE items(price DECIMAL(5,2));
+ 
+INSERT INTO items(price) VALUES(7);
+ 
+ --won't be inserted. error out because it's too large
+INSERT INTO items(price) VALUES(7987654);
+ 
+INSERT INTO items(price) VALUES(34.88);
+ 
+INSERT INTO items(price) VALUES(298.9999);
+ 
+INSERT INTO items(price) VALUES(1.9999);
+ 
+SELECT * FROM items;
 
 
 ------------------------------------------------------
