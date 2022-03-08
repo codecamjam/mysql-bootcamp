375df2e364114b5ec814e54f41fc0b31f1046ff2
codecamjam <camerontaghlabi@gmail.com>
2022-03-07 20:19:14 -0500

    l225 TV Joins Challenge 1

--- sec13/many-to-many.sql
+++ sec13/many-to-many.sql
@@ -79,7 +79,33 @@ INSERT INTO series (title, released_year, genre) VALUES
 
 /*l225 TV Joins Challenge 1
 *****************************************************/
++----------------------+--------+
+| title                | rating |
++----------------------+--------+
+| Archer               |    8.0 |
+| Archer               |    7.5 |
+| Archer               |    8.5 |
+| Archer               |    7.7 |
+| Archer               |    8.9 |
+| Arrested Development |    8.1 |
+| Arrested Development |    6.0 |
+| Arrested Development |    8.0 |
+| Arrested Development |    8.4 |
+| Arrested Development |    9.9 |
+| Bob's Burgers        |    7.0 |
+| Bob's Burgers        |    7.5 |
+| Bob's Burgers        |    8.0 |
+| Bob's Burgers        |    7.1 |
+| Bob's Burgers        |    8.0 |
++----------------------+--------+
 
+SELECT title, rating
+FROM series 
+JOIN reviews
+  ON series.id = reviews.series_id;
+
+--he uses limit 15 but for our example print everything
+--join series and reviews
 
 
 ------------------------------------------------------
