3912ceb7a42d93554e8a2b028a11355cf7987a7e
codecamjam <camerontaghlabi@gmail.com>
2022-03-07 21:07:50 -0500

    l235 tv joins challenge 6

--- sec13/many-to-many.sql
+++ sec13/many-to-many.sql
@@ -230,7 +230,46 @@ GROUP BY genre;
 
 /*l235 tv joins challenge 6
 *****************************************************/
++------------+-----------+-------+-----+-----+---------+----------+
+| first_name | last_name | COUNT | MIN | MAX | AVG     | STATUS   |
++------------+-----------+-------+-----+-----+---------+----------+
+| Thomas     | Stoneman  |     5 | 7.0 | 9.5 | 8.02000 | ACTIVE   |
+| Wyatt      | Skaggs    |     9 | 5.5 | 9.3 | 7.80000 | ACTIVE   |
+| Kimbra     | Masters   |     9 | 6.8 | 9.0 | 7.98889 | ACTIVE   |
+| Domingo    | Cortes    |    10 | 5.8 | 9.1 | 7.83000 | ACTIVE   |
+| Colt       | Steele    |    10 | 4.5 | 9.9 | 8.77000 | ACTIVE   |
+| Pinkie     | Petit     |     4 | 4.3 | 8.8 | 7.25000 | ACTIVE   |
+| Marlon     | Crafford  |     0 | 0.0 | 0.0 | 0.00000 | INACTIVE |
++------------+-----------+-------+-----+-----+---------+----------+
 
+SELECT first_name, 
+       last_name, 
+       Count(rating)                               AS COUNT, 
+       Ifnull(Min(rating), 0)                      AS MIN, 
+       Ifnull(Max(rating), 0)                      AS MAX, 
+       Round(Ifnull(Avg(rating), 0), 2)            AS AVG, 
+       IF(Count(rating) > 0, 'ACTIVE', 'INACTIVE') AS STATUS 
+FROM   reviewers 
+       LEFT JOIN reviews 
+              ON reviewers.id = reviews.reviewer_id 
+GROUP  BY reviewers.id; 
+-- CHALLENGE 6 - Reviewer Stats With POWER USERS 
+
+SELECT first_name, 
+       last_name, 
+       Count(rating)                    AS COUNT, 
+       Ifnull(Min(rating), 0)           AS MIN, 
+       Ifnull(Max(rating), 0)           AS MAX, 
+       Round(Ifnull(Avg(rating), 0), 2) AS AVG, 
+       CASE 
+         WHEN Count(rating) >= 10 THEN 'POWER USER' 
+         WHEN Count(rating) > 0 THEN 'ACTIVE' 
+         ELSE 'INACTIVE' 
+       end                              AS STATUS 
+FROM   reviewers 
+       LEFT JOIN reviews 
+              ON reviewers.id = reviews.reviewer_id 
+GROUP  BY reviewers.id; 
 
 
 ------------------------------------------------------
