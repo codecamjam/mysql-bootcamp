960f8548c84a88336211c63ce14f68c85ce2ab80
codecamjam <camerontaghlabi@gmail.com>
2022-03-12 08:36:24 -0500

    l243 cloning instagram's db: photos schema

--- sec14/instagram-db-clone.sql
+++ sec14/instagram-db-clone.sql
@@ -20,7 +20,32 @@ INSERT INTO users (username) VALUES
 
 
 
-/*l
+/*l243 cloning instagram's db: photos schema
+*****************************************************/
+
+CREATE TABLE photos (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    image_url VARCHAR(255) NOT NULL,
+    user_id INTEGER NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    FOREIGN KEY(user_id) REFERENCES users(id)
+);
+
+INSERT INTO photos (image_url, user_id) VALUES 
+('/alskjd76', 1)
+('/lkajsd98', 2)
+('/90jddlkj', 3);
+
+SELECT photos.image_url, users.username
+FROM photos
+JOIN users
+  ON photos.user_id = users.id;
+
+------------------------------------------------------
+
+
+
+/*l245 cloning instagram's db: comments schema
 *****************************************************/
 
 
@@ -28,7 +53,7 @@ INSERT INTO users (username) VALUES
 
 
 
-/*l
+/*l247 cloning instagram's db: likes schema
 *****************************************************/
 
 
@@ -36,15 +61,7 @@ INSERT INTO users (username) VALUES
 
 
 
-/*l
-*****************************************************/
-
-
-------------------------------------------------------
-
-
-
-/*l
+/*l249 cloning instagram's db: followersschema
 *****************************************************/
 
 
