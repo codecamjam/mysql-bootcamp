d2398fdd98be918fb5f81c717685d84c4e761499
codecamjam <camerontaghlabi@gmail.com>
2022-03-12 09:07:09 -0500

    l249 cloning instagram's db: followers schema

--- sec14/instagram-db-clone.sql
+++ sec14/instagram-db-clone.sql
@@ -87,9 +87,25 @@ INSERT INTO likes(user_id, photo_id) VALUES
 
 
 
-/*l249 cloning instagram's db: followersschema
+/*l249 cloning instagram's db: followers schema
 *****************************************************/
+CREATE TABLE follows (
+    follower_id INTEGER NOT NULL,
+    followee_id INTEGER NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    FOREIGN KEY(follower_id) REFERENCES users(id),
+    FOREIGN KEY(followee_id) REFERENCES users(id),
+    PRIMARY KEY(follower_id, followee_id)
+);
 
+INSERT INTO follows(follower_id, followee_id) VALUES
+(1,2),
+(1,3),
+(3,1),
+(2,3);
+
+INSERT INTO follows(follower_id, followee_id) VALUES
+(2,1);
 
 ------------------------------------------------------
 
